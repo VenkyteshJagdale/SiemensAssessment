03913a7efbd7a57feb11773459fae79d8f49570e
VenkyteshJagdale <venkytesh.jagdale1997@gmail.com>
2024-07-17 19:27:52 +0530

    Db changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-//Database Connection
+//Database Connections
 const connectDB = async () => {
     try {
         await mongoose.connect("mongodb://localhost:27017/userRegi");
